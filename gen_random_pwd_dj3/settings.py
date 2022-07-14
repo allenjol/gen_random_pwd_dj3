226e9c110d08b74a5feb85d90d648ef67fe82406
allen_jol <allen_jol@qq.com>
2022-07-14 17:03:45 +0800

    更新环境变量名

--- gen_random_pwd_dj3/settings.py
+++ gen_random_pwd_dj3/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dzacq!xc5dwm65x4yv%j!bpnufnas#k+8i9*hbp(cjh&2$$(#1')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-dzacq!xc5dwm65x4yv%j!bpnufnas#k+8i9*hbp(cjh&2$$(#1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
